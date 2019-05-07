e95d6f669c02a2dfa6b8146924655c851c8fc173
Enes koç <32553624+enskocc12@users.noreply.github.com>
2019-05-07 15:36:53 +0300

    Add files via upload

---
+++ post/post.component.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  constructor(private http:HttpClient) {  }
+
+  path:string="https://jsonplaceholder.typicode.com/posts"
+  posts:Post[];
+
+  ngOnInit() {
+    this.http.get<Post[]>(this.path).subscribe(response=>{
+      this.posts=response;
+    });
+    
+  }
+
+}
---
+++ post/post.ts
@@ -0,0 +1,6 @@
+export class Post{
+    userid:number;
+    id:number;
+    title:string;
+    body:string;
+}
